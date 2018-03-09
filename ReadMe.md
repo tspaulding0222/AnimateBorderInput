3f949a067afc79c37cac383815bc521b3d8cfa10
Thomas Spaulding <tspauldi@assurerx.local>
2018-03-09 10:30:30 -0500

    readded missing files

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Test Site and Gulp Setup
+
+## Things to know
+* Chrome Gulp-Connect HTTP to HTTPS problem
+    * May have to delete domain security policies
+        * chrome://net-internals/#hsts
+        * Delete Domain Security Policies for localhost
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+
+/* File: gulpfile.js */
+var babelify = require('babelify');
+var gulp  = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var bro = require('gulp-bro');
+var connect = require('gulp-connect');
+
+gulp.task('js', function() {
+    gulp
+      .src("./source/js/app.js")
+      .pipe(bro({
+          transform: [
+            babelify.configure({
+              presets: [
+                ["env", {
+                    "targets": {
+                      "browsers": ["last 2 versions", "ie >= 9"]
+                    }
+                }]
+              ]
+            })
+          ]
+        }))
+      .pipe(gulp.dest("./dist/js"))
+      
+})
+
+gulp.task('scss', function() {
+    gulp.src('./source/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/styles'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function() {
+    gulp.src('./source/index.html')
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./source/scss/*.scss', ['scss']);
+    gulp.watch('./source/js/*.js', ['js']);
+    gulp.watch('./source/*.html', ['html']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: './dist',
+    port: 8888,
+    livereload: true
+  });
+});
+
+gulp.task('default', ['connect', 'watch']);
